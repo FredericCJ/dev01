6d69f42ea297226bb959f03dbf80a11946ae95f0
FredericCJ <frederic.ferragne@hotmail.com>
2021-09-07 10:56:27 +0200

    Saisie min max

---
+++ sujet.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int min(int *);
+int max(int *); 
+
+int main(int argc, char *argv[]){
+    int i = 0;
+    int notes[13] = {0};
+    
+    for(int j=0; j<13; j++){
+        notes[j] = 21;
+    }
+    
+    char prenom[10];
+    char cas_terminant[10] = "X AE A-XII";
+    
+    while(prenom != cas_terminant){
+        printf("Saisir prenom : ");
+        scanf("%s",prenom);
+        while(notes[i]<0 || notes[i]>20){
+            printf("Saisir note : ");
+            scanf("%d",&notes[i]);
+        }
+        i++;
+        printf("%s\n",prenom);
+    }
+    printf("%d\n%d\n",min(notes),max(notes));
+    return EXIT_SUCCESS;
+}
+
+int min(int *notes){
+    int i = 0;
+    int max = 20;
+    while(notes[i] != '\0'){
+        if(notes[i] <= max){
+            notes[i] = max;
+        }
+    }
+    return max;
+}
+
+int max(int *notes){
+    int i = 0;
+    int min = 0;
+    while(notes[i] != '\0'){
+        if(notes[i] >= min){
+            notes[i] = min;
+        }
+    }
+    return min;
+}
